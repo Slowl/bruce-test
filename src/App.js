07d7f9144661d56c2b430e6d12415fbe4e000ce9
Slowl <hedi0475@gmail.com>
2020-01-13 22:39:01 +0100

    added button to load next elements and loader indicator

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
 import md5 from 'md5'
-import {Router} from '@reach/router'
+import { Router } from '@reach/router'
 import MarvelList from './pages/MarvelList'
 import HeroPage from './pages/HeroPage'
 
 const AppContainer = styled.div`
   width: 100%;
   min-height: 100vh;
+  padding-bottom: 4em;
   background-color: #f2f2f2;
   overflow-x: hidden;
 `
 
 class App extends React.Component {
   state = {
-    data: {},
+    data: [],
     selectedElem: {
       name: '',
       description:'',
       imgUrl: '',
       comics: {},
       series: {}
-    }
+    },
+    offset: 0,
+    loading: false,
   }
 
   componentDidMount(){
@@ -32,7 +35,24 @@ class App extends React.Component {
     fetch(`https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${apikey}&hash=${hash}`)
     .then(res => res.json())
     .then(resData => {
-      this.setState({data: resData.data}, () => console.log(this.state.data))
+      this.setState({data: resData.data.results, offset: 20}, () => console.log(this.state.data))
+    })
+  }
+
+  nextResult = e => {
+    const ts = `${Date.now()}`
+    const apikey = "41d8b2187390c7255bd3cd8c034bf8c3"
+    const privatekey = "4656fb7bbef9379ebd1c6d5851dadc379160a6e2"
+    const hash = md5(ts + privatekey + apikey)
+    this.setState({ loading: true })
+    fetch(`https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${apikey}&hash=${hash}&offset=${this.state.offset}`)
+    .then(res => res.json())
+    .then(resData => {
+      this.setState(prevState => ({
+        data: [...prevState.data, ...resData.data.results],
+        offset: prevState.offset + 20,
+        loading: false
+      }))
     })
   }
 
@@ -50,7 +70,7 @@ class App extends React.Component {
     return(
       <AppContainer>
         <Router>
-          <MarvelList data={this.state.data.results} handleClick={this.handleElemClick} path="/" />
+          <MarvelList data={this.state.data} handleClick={this.handleElemClick} next={this.nextResult} loading={this.state.loading} path="/" />
           <HeroPage path="characters/:id" selectedHero={this.state.selectedElem} />
         </Router>
 
--- src/pages/MarvelList.jsx
+++ src/pages/MarvelList.jsx
@@ -7,26 +7,66 @@ const ListContainer = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   width: 100vw;
+  padding-bottom: 2em;
 `
+const Next = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1em;
+  margin-top: 8em;
+  background-color: white;
+  border-radius: 8px;
+  text-align: center;
+  letter-spacing: 1px;
+  width: 20%;
+  margin: auto;
+  cursor: pointer;
+  transition: .3s;
 
-const MarvelList = ({ data, handleClick }) => {
+  :hover {
+    background-color: #323232;
+    color: #f4f4f4;
+  }
+
+  :after{
+    display: inline-block;
+    margin-left: 2em;
+    content: '';
+    width: 6px;
+    height: 6px;
+    border-radius: 50px;
+    background-color: #6ef76f;
+    opacity: 0;
+    animation: ${props => props.loading ? ".3s linear 0s infinite alternate loader" : "0s linear 0s infinite alternate loader"};
+
+    @keyframes loader {
+      from {opacity: 1;}
+      to {opacity: 0;}
+    }
+  }
+`
+const MarvelList = ({ data, handleClick, next, loading }) => {
 
   return (
-    <ListContainer>
-      {data && data.map(heroes => {
-        return (
-          <MarvelCard
-            key={heroes.id}
-            name={heroes.name}
-            imgUrl={heroes.thumbnail.path}
-            description={heroes.description}
-            comics={heroes.comics}
-            series={heroes.series}
-            id={heroes.id}
-            onClick={handleClick} />
-        )
-      })}
-    </ListContainer>
+    <div>
+      <ListContainer>
+        {data && data.map(heroes => {
+          return (
+            <MarvelCard
+              key={heroes.id}
+              name={heroes.name}
+              imgUrl={heroes.thumbnail.path}
+              description={heroes.description}
+              comics={heroes.comics}
+              series={heroes.series}
+              id={heroes.id}
+              onClick={handleClick} />
+          )
+        })}
+      </ListContainer>
+      <Next onClick={() => next()} loading={loading}>LOAD MORE</Next>
+    </div>
   )
 }
 
